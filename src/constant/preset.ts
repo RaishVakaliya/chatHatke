bdb19c131abcc3243130fe8fa532f9044393e974
Raish Vakaliya <163170872+RaishVakaliya@users.noreply.github.com>
2026-03-16 15:00:42 +0530

    create preset

---
+++ src/constant/preset.ts
@@ -0,0 +1,14 @@
+export const PRESETS = [
+  "Busy",
+  "Free to Chat",
+  "Sleeping",
+  "Vibing",
+  "Travelling",
+  "At Work",
+  "In a meeting",
+  "Gaming",
+  "Battery about to die",
+  "Can't talk, ChatHatke only",
+  "Debugging life",
+  "Hatke mood",
+];
